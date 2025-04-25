7d04fb5c02c7cc58179d142e709fb208a73b4b55
Patrick Cole <patrickbgcole@icloud.com>
2025-04-25 18:22:24 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . f f f f f e f . e 4 4 . . 
+    . . f f e e f e e e f 4 d 4 . . 
+    . f f f e 2 f f 4 e e f f f . . 
+    . f f e 2 f f 4 4 d e 2 2 4 f f 
+    f f 2 e 2 f e 4 4 d 4 2 2 4 f f 
+    f 2 2 e 2 f e 4 d d 4 2 2 5 f . 
+    f 2 2 e 2 f e 4 d d 4 2 2 5 f . 
+    f f 2 e 2 f e 4 4 d 4 2 2 4 f f 
+    . f f e 2 f f 4 4 d e 2 2 4 f f 
+    . f f f e 2 f f 4 e e f f f . . 
+    . . f f f e f e e e f 4 d 4 . . 
+    . . . f f f f f e f . e 4 4 . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
